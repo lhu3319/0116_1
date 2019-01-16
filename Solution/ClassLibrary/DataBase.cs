5e5f06d74e066a35353a10af45e779236f6207d2
Han <lhu3319@nate.com>
2019-01-16 11:07:13 +0900

    123

---
+++ Solution/ClassLibrary/DataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace ClassLibrary
+{
+    public class DataBase
+    {
+        private string strConn1 = string.Format("server={0};user={1};password={2};database={3};", "192.168.3.130", "root", "1234", "test");
+        private string strConn2 = string.Format("server={0};user={1};password={2};database={3};", "192.168.3.151", "root", "1234", "ubun");
+
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection();
+                conn.ConnectionString = strConn1;
+                conn.Open();
+                return conn;
+            }
+            catch ( MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
